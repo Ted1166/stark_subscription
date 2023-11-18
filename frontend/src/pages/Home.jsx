4e9c6d51b18739c356a343d26186e0f454034b95
Ted1166 <103740031+Ted1166@users.noreply.github.com>
2023-11-18 04:59:05 +0000

    Adev funtime

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,13 +5,18 @@ const Home = () => {
   return (
     <div>
       <Grid>
+      <Grid.Col span={{ md: 6 }}>
+          <Image src={'/ui/Adev.png'} maw={'90%'} radius={'md'} />
+        </Grid.Col>
+      
         <Grid.Col span={{ md: 6 }}>
           <Stack className='h-100' justify='center'>
-            <Title>Adev</Title>
+            <h1>Welcome to ADEV FUNTIME - Your Gateway to Endless Entertainment! 
+              Subscribe to an array of captivating channels curated by talented content creators. 
+              Dive into a world of diverse content, from thrilling adventures to insightful discussions. 
+              Discover, subscribe, and immerse yourself in a universe of entertainment tailored just for 
+              you. Join ADEV FUNTIME today and embark on an exhilarating journey of endless discovery!</h1>
           </Stack>
-        </Grid.Col>
-        <Grid.Col span={{ md: 6 }}>
-          <Image src={'/ui/Adev.png'} maw={'90%'} radius={'md'} />
         </Grid.Col>
       </Grid>
     </div>
